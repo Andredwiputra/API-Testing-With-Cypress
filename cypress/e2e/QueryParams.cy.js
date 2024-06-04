0e0e309130d727117ca857068d953c3bc0a005c0
Andredwiputra <44828486+Andredwiputra@users.noreply.github.com>
2024-06-04 11:39:16 +0800

    new query params

---
+++ cypress/e2e/QueryParams.cy.js
@@ -0,0 +1,22 @@
+describe("API testing", ()=> {
+
+    it("Passing Query parameters", ()=>{
+
+        cy.request({
+            method: 'GET',
+            url: 'https://reqres.in/api/users',
+            qs: { page:1 }
+        })
+        .then( (response) => {
+            expect(response.status).to.eq(200);
+            expect(response.status).equal(200);
+            expect(response.body.page).to.eq(1);
+            expect(response.body.data).has.length(6);
+            expect(response.body.data[1]).have.property('id',2);
+            expect(response.body.data[1]).have.property('first_name', 'Janet');
+        })
+
+    })
+
+
+})
